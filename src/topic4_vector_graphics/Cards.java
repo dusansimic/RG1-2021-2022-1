2502ab2a1731747367515f0a22f50f9e498755f3
Dušan Simić <dusan.simic1810@gmail.com>
2021-11-29 12:25:02 +0100

    Dodato resenje za karte

--- src/topic4_vector_graphics/Cards.java
+++ src/topic4_vector_graphics/Cards.java
@@ -1,12 +1,16 @@
 package topic4_vector_graphics;
 
+import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
 import mars.drawingx.application.DrawingApplication;
 import mars.drawingx.drawing.Drawing;
 import mars.drawingx.drawing.DrawingUtils;
 import mars.drawingx.drawing.View;
 import mars.drawingx.gadgets.annotations.GadgetInteger;
+import mars.geometry.Transformation;
 import mars.geometry.Vector;
+import mars.random.RNG;
 
 
 public class Cards implements Drawing {
@@ -24,11 +28,18 @@ public class Cards implements Drawing {
 	
 	void drawCard(View view) {
 		// Okvir karte sa senkom
-		
+		view.setFill(Color.WHITE);
+		view.setEffect(new DropShadow(32, Color.BLACK));
+		view.fillRoundRectCentered(Vector.ZERO, size.div(2), a);
+		view.setEffect(null);
 
 
 		// Simbol na karti
-		
+		view.setLineWidth(16);
+		view.setStroke(Color.RED);
+		view.strokeCircle(Vector.ZERO, r);
+		view.setLineCap(StrokeLineCap.BUTT);
+		view.strokeLine(Vector.polar(r, 1.0/8), Vector.polar(r, 5.0/8));
 
 	}
 	
@@ -36,7 +47,16 @@ public class Cards implements Drawing {
 	@Override
 	public void draw(View view) {
 		DrawingUtils.clear(view, Color.hsb(120, 0.5, 0.2));
-		
+
+		RNG rng = new RNG(123456789);
+
+		for (int i = nCards - 1; i >= 0; i--) {
+			view.setTransformation(new Transformation()
+				.rotate(0.01 * rng.nextGaussian())
+				.translate(d.mul(i).sub(d.mul(nCards).div(2)))
+			);
+			drawCard(view);
+		}
 	}
 	
 	
