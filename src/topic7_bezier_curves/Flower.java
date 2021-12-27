6b0f7a4638714a1e36084df5eb04992f6878c745
Dušan Simić <dusan.simic1810@gmail.com>
2021-12-27 10:37:02 +0100

    Dodat cvet

--- src/topic7_bezier_curves/Flower.java
+++ src/topic7_bezier_curves/Flower.java
@@ -19,12 +19,23 @@ public class Flower implements Drawing {
 	Vector o  = new Vector( 0, -200);  // Koren cveta
 	
 	// levi list
-
+	Vector ll1 = new Vector(-150, -100);
+	Vector ll2 = new Vector(-100, 0);
+	Vector l = new Vector(-150, 100);
+	Vector lr1 = new Vector(-100, 50);
+	Vector lr2 = new Vector(0, -100);
 	
 	// stabljika
+	Vector s1 = new Vector(0, 0); // kontrolna tacka 1
+	Vector s2 = new Vector(100, 100); // kontrolna tacka 2
 
 
 	// desni list
+	Vector rl1 = new Vector(0, -100);
+	Vector rl2 = new Vector(200, -100);
+	Vector r = new Vector(250, 0);
+	Vector rr1 = new Vector(250, -100);
+	Vector rr2 = new Vector(100, -200);
 
 	
 	// cvet
@@ -43,25 +54,54 @@ public class Flower implements Drawing {
 	public void draw(View view) {
 		DrawingUtils.clear(view, Color.gray(0.125));
 		
+		Color green = Color.rgb(116, 156, 118);
 		// levi list
-		
+		view.beginPath();
+		view.moveTo(o);
+		view.bezierCurveTo(ll1, ll2, l.add(Vector.polar(20, -0.3 * time)));
+		view.bezierCurveTo(lr1, lr2, o);
+		view.setFill(green);
+		view.fill();
 
 
 		// stabljika
-		
+		Vector c = s2.add(Vector.polar(30, 0.5 * time));
+		view.beginPath();
+		view.moveTo(o);
+		view.quadraticCurveTo(s1, c);
+
+		view.setLineWidth(12);
+		view.setStroke(green);
+		view.stroke();
 
 		
 		// desni list
-		
+		view.beginPath();
+		view.moveTo(o);
+		view.bezierCurveTo(rl1, rl2, r.add(Vector.polar(20, 0.3 * time)));
+		view.bezierCurveTo(rr1, rr2, o);
+		view.setFill(green);
+		view.fill();
 
 
 
 		// latice
-		
+		for (int i = 0; i < nPetals; i++) {
+			Vector p1 = Vector.polar(rDisk, i * 1.0 / nPetals);
+			Vector p2 = Vector.polar(rDisk + lPetal, i * 1.0 / nPetals);
+			Vector p3 = Vector.polar(rDisk + lPetal, (i + 1) * 1.0 / nPetals);
+			Vector p4 = Vector.polar(rDisk, (i + 1) * 1.0 / nPetals);
+			view.beginPath();
+			view.moveTo(c.add(p1));
+			view.bezierCurveTo(c.add(p2), c.add(p3), c.add(p4));
+			view.setFill(Color.rgb(156, 116, 153));
+			view.fill();
+		}
 		
 
 		// centar cveta
-		
+		view.setFill(Color.rgb(154, 156, 116));
+		view.fillCircleCentered(c, rDisk);
 
 	}
 	
